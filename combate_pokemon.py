ff849405b7c5a0575c8c34599e62fe130f85cb0c
Adrián <la.muerte.de.pandora@gmail.com>
2018-09-23 15:01:32 +0200

    combate pokemon segunda fase mejor optimizado

--- combate_pokemon.py
+++ combate_pokemon.py
@@ -4,45 +4,45 @@ vida_pikachu = 100
 vida_enemigo = 0
 
 if pokemon_elegido == "RATATA":
-    vida_enemigo = 80
+     vida_enemigo = 80
+     dano_enemigo = 9
+     nombre_enemigo = "ratata"
 
-if pokemon_elegido == "MEW":
-    vida_enemigo = 70
+elif pokemon_elegido == "MEW":
+     vida_enemigo = 70
+     dano_enemigo = 14
+     nombre_enemigo = "mew"
 
-if pokemon_elegido == "CHARMANDER":
-    vida_enemigo = 100
+elif pokemon_elegido == "CHARMANDER":
+     vida_enemigo = 100
+     dano_enemigo = 20
+     nombre_enemigo = "charmander"
 
 while vida_pikachu > 0 and vida_enemigo > 0:
 
     ataque_elegido = input("¿que ataque quieres elegir? (CHISPAZO / BOLA VOLTIO): ").upper()
 
     if ataque_elegido == "CHISPAZO":
-        vida_enemigo -= 10
+        print("Pikachu usa ataque chspazo y te hace un daño de 16")
+        vida_enemigo -= 16
 
-    if ataque_elegido == "BOLA VOLTIO":
-        vida_enemigo -= 13
+    elif ataque_elegido == "BOLA VOLTIO":
+        print("Pikachu usa ataque bola voltio y te hace un daño de 24")
+        vida_enemigo -= 24
 
-    print("la vida del enemigo es de {}".format(vida_enemigo))
+    print("la vida de {} es de {}".format(nombre_enemigo, vida_enemigo))
+    print("{} te hace un ataque {} de daño".format(nombre_enemigo, dano_enemigo))
+    vida_pikachu -= dano_enemigo
+    print("la vida de pikachu es de {}".format(vida_pikachu))
 
-    if pokemon_elegido == "RATATA":
-        print("Ratata te hace un ataque de 5 de daño!")
-        vida_pikachu -= 5
-
-    if pokemon_elegido == "MEW":
-        print("Mew te hace un ataque de 10 de daño!")
-        vida_pikachu -= 10
-
-    if pokemon_elegido == "CHARMANDER":
-        print("Charmander te hace un ataque de 20 de daño!")
-        vida_pikachu -= 20
-
-    print("La vida de pikachu es de {}".format(vida_pikachu))
 
 if vida_enemigo <= 0:
     print("has ganado")
+    resultado = print("el combate a finalizado")
+    input(resultado), input(exit())
 
 if vida_pikachu <= 0:
     print("has perdido")
 
 
-print("el combate a termionado")
+
